b320a5c4fb2af36ee4f1c6de61ad3d0f745430cc
wensen.lws <wensen.lws@alibaba-inc.com>
2014-05-11 18:11:06 +0800

    add bin/

---
+++ bin/setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -e $HOME/repo/toolbox ]; then
+    rm -rf $HOME/repo/toolbox
+fi
+
+mkdir $HOME/repo
+cd $HOME/repo
+
+git checkout git@github.com:leungwensen/toolbox.git
+
+cd $HOME
+
+ln -s $HOME/repo/toolbox/scripts scripts
+
+ln -s $HOME/repo/toolbox/dotfiles/.vimrc   .vimrc
+ln -s $HOME/repo/toolbox/dotfiles/.vimrc.d .vimrc.d
+
+ln -s $HOME/repo/toolbox/dotfiles/.bash_profile   .bash_profile
+ln -s $HOME/repo/toolbox/dotfiles/.bash_profile.d .bash_profile.d
+
+ln -s $HOME/repo/toolbox/dotfiles/.vimperatorrc .vimperatorrc
+
+ln -s $HOME/repo/toolbox/dotfiles/.gitignore .gitignore
+
+source $HOME/.bash_profile
+
